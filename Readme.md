7638deba71da31758bfec1eb381818c7e58585a6
Johnny IV Young O <asesordeprogramacion@gmail.com>
2019-06-20 10:57:21 -0500

    implemented mongodb case

--- Readme.md
+++ Readme.md
@@ -3,8 +3,17 @@
 Requisitos:
 
 instalar artillery globalmente
+```
 npm install artillery -g
+```
 
+condiciones de chequeos
+
+```
+node --version => 8 or higher
+npm --version => 6 or higher
+docker --version => 18 or higher
+```
 
 Herramientas utilizadas
 
@@ -13,7 +22,9 @@ ExpressJS
 	npm install
 	npm run dev
 	hay varias rutas
-		http://localhost:3001
+		http://localhost:3001 es un hola mundo
+		http://localhost:3001/load  carga data en MongoDB
+		http://localhost:3001/carts/<id> retorna el precio total del carrito, basado en su ID
 
 NextJS
 	ir dentro de la carpeta /next: cd next
@@ -21,6 +32,7 @@ NextJS
 	npm run dev
 	hay dos tipo de páginas
 		una con http://localhost:3000 y http://localhost:300/ioblock
+
 
 Artillery https://artillery.io/
 	los escenesarios de carga son creado en la carpeta artillery.
--- express/index.js
+++ express/index.js
@@ -3,10 +3,91 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+const {
+	MongoClient
+} = require('mongodb');
+
+const port = process.env.PORT || 3001;
+//mongodb://[username:password@]host1[:port1][,...hostN[:portN]]][/[database][?options]]
+const mongoUri = 'mongodb://mongoadmin:secret@localhost:27017';
+
+function getConnectionMongo() {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(mongoUri, { useNewUrlParser: true } , (err, client) => {
+			if (err) {
+				reject(err);
+			}
+			else{
+				resolve(client);
+			}
+		});
+	});
+}
+
+app.get('/', function ping(req, res) {
+	res.send('Hello World!');
 });
 
-app.listen(3001, function () {
-  console.log('app listening on port 3001!');
+app.get('/load', async function loadData(req, res, next) {
+	let count = 0;
+	const max = 10000;
+	try {
+		const client = await getConnectionMongo();
+
+		const db = client.db('workshop');
+		const collection = db.collection('carts');
+
+		function insert(err) {
+			if (err) throw err;
+
+			if (count++ === max) {
+				return client.close();
+			}
+
+			collection.insertOne({
+				cart: parseInt(Math.random() * 100),
+				quantity: parseInt(Math.random() * 10) + 1,
+				price: Math.random() * 1000,
+			}, insert);
+		}
+
+		insert();
+		res.send('load exitoso');
+	} catch (error) {
+		next(error);
+	}
+});
+
+app.get('/carts/:id', async function getCartsById(req, res, next) {
+	try {
+		const client = await getConnectionMongo();
+
+		const db = client.db('workshop');
+		const collection = db.collection('carts');
+		const cart = parseInt(req.paramw.id);
+
+    collection.find({ cart }).toArray((err, results) => {
+
+      const total = results.reduce((a, e) => a += e.price * e.quantity, 0);
+
+      res.send({ total });
+    });
+	} catch (error) {
+		next(error);
+	}
+
+});
+
+app.get('/usersoptimized', function (req, res) {
+	res.send('users optimized');
+});
+
+app.listen(port, function init() {
+	console.log(`app listening on port ${port}!`);
+});
+
+app.use(function handleError(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
