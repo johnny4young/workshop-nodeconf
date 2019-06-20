ed4a5fdad9a9e8607af96020bac68267ca83111e
Johnny IV Young O <asesordeprogramacion@gmail.com>
2019-06-19 20:37:38 -0500

    added nextjs case

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# WorkShop NodeConf
+
+Requisitos:
+
+instalar artillery globalmente
+npm install artillery -g
+
+
+Herramientas utilizadas
+
+ExpressJS
+	TODO
+NextJS
+	ir dentro de la carpeta /next
+	npm install
+	npm run dev
+	hay dos tipo de de paginas
+		una con localhost:3000 y localhost:300/ioblock
+
+Artillery https://artillery.io/
+	creado el load test por npm, tarea: agregarlo como escenarios por ser más descriptivos
---
+++ next/pages/ioblock.js
@@ -0,0 +1,30 @@
+const Ioblock = (props) => (
+	<div>
+	  <p>Hello Next.js
+	  {props.time.map(date => (
+		<b> {date.toString()} </b>
+	  ))}
+	  </p>
+	  {props.table.map(row => (
+		<tr> oh my {row}</tr>
+	  ))}
+	</div>
+  )
+  
+  Ioblock.getInitialProps = async function() {
+	const data = [ Date.now() ]
+	const table = []
+  
+	const factor = Date.now() / 10000 % 1 > 0.5 ? 1000 : 10000
+  
+	for (let i = 0; i < 1e6; i++) {
+	  table.push(`Welcome to row ${i}`)
+	}
+  
+	return {
+	  time: data,
+	  table: table
+	}
+  }
+  
+  export default Ioblock
